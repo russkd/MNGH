79ed696cc4ce27366b40ab215233fcd9f4798dc1
russkd <russ.dollinger@mynextgenhealth.net>
2016-02-27 15:59:27 -0800

    deleted misnamed controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/help'
-
-  get 'home/about'
-
-  get 'home/contact'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
