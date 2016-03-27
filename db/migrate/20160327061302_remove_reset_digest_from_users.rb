bd7972bcf4a7129f184bc4dc7e819dc52418704f
russkd <russ.dollinger@mynextgenhealth.net>
2016-03-26 23:17:06 -0700

    modified to migrations to fix users table

---
+++ db/migrate/20160327061302_remove_reset_digest_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveResetDigestFromUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :reset_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324230108) do
+ActiveRecord::Schema.define(version: 20160327061526) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160324230108) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
-    t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
