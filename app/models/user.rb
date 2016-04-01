e5db11c58d54fa5ed9e7ab77bb8f3422d9bad5d0
russkd <russ.dollinger@mynextgenhealth.net>
2016-04-01 13:19:38 -0700

    Corrections to the activate method and create reserve digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ end
   end
 
   def activate
-    update_columns(:activated, true, :activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
@@ -59,8 +59,8 @@ end
   # Creates password reset attributes
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(:reset_digest, User.digest(reset_token),
-                  :reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                  reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset via email.
