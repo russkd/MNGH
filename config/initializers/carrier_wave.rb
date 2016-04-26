aa5c4f4d1057e604544cd317779e3066eaa53e4c
russkd <russ.dollinger@mynextgenhealth.net>
2016-04-26 14:11:19 -0700

    Updated gemfile and changed carrierwave name

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
     :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
     :region                 => 'us-west-2',
-    # :host                   => 's3.example.com',# optional, defaults to nil
+    :host                   => 'mynextgenhealth.s3-us-west-2.amazonaws.com',# optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory      =  ENV['S3_BUCKET']
