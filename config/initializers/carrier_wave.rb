07c68485c4bcf770fbf822d72d451783b10ea13f
russkd <russ.dollinger@mynextgenhealth.net>
2016-04-25 20:10:05 -0700

    Changes to carrier wave config file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,12 @@ if Rails.env.production?
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-    :region                 => ENV['S3_REGION']
+    :region                 => 'us-west-2',
+    :host                   => 's3.example.com',# optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory      =  ENV['S3_BUCKET']
+  config.fog_public         =  false
+  config.fog_attributes     =  {'Cache-Control'=>'max-age=315576000'}
   end
 end
