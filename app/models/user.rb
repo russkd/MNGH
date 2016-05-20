806ad9866296ec3239219e9b49cc9262cfef5884
russkd <russ.dollinger@mynextgenhealth.net>
2016-05-19 22:43:06 -0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,9 +87,11 @@ end
   end
 
   # Defines a proto-feed
-  # See following users for full-implementation
+  # Returns a user's status feed
   def feed
-    Micropost.where('user_id = ?', id)
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",  user_id: id)
   end
 
   def follow(other_user)
